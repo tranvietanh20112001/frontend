941b009b486efb5d59389c6c506554cbaacbe41f
Sierra <huongbtgch200638@fpt.edu.vn>
2024-01-16 10:57:14 +0700

    fix header

--- src/components/Header/HeaderComponent.tsx
+++ src/components/Header/HeaderComponent.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
-
 import * as S from "./Header.styled";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
@@ -29,25 +28,22 @@ const HeaderComponent: React.FC = () => {
   const cart = useSelector((state: RootState) => state.cart.cart);
   const totalQuantity = cart.length;
 
-
   return (
-
     <Navbar bg="light" data-bs-theme="light" style={{ padding: "0" }}>
       <Container>
         <Nav>
           <NavLink to="#home">Home</NavLink>
-
-    <Navbar bg="light" data-bs-theme="light" style={{ padding: '0' }}>
-      <Container>
-        <Nav>
-        <NavLink to="#home">Home</NavLink>
-
-
+          <NavLink to="/products">Products</NavLink>
+          <NavLink to="#pricing">Review</NavLink>
+        </Nav>
+        <Navbar.Brand href="#home">
+          <S.NavbarLogo>Beauty.bd</S.NavbarLogo>
+        </Navbar.Brand>
+        <NavLink to="/cart">
           <GreenCartIcon>
             <span>{totalQuantity}</span>
             <i className="bi bi-cart"></i>
           </GreenCartIcon>
-
         </NavLink>
       </Container>
     </Navbar>
